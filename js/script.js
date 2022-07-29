353186d5783a427d9c94eaa4c95812dbd259f965
Gabriel Abranches <103786112+gafbra@users.noreply.github.com>
2022-07-29 16:55:59 -0300

    apliquei jquery no projeto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
+
+$(document).ready(function (){
+    //todas as suas funções jQuery
+    $(".btn-buy").click(function(){
+        alert("Este produto não está disponível!");
+    });
+});
+
 function abreFechaMenu() {
     let navbar = document.querySelector(".menu");
-    navbar.classList.toggle("mostraEscondeMenu")
+    navbar.classList.toggle("mostraEscondeMenu");
  }
